96fb68cb329a7caa1676c4149f4a1a3904a307a5
Tylr-J42 <Tyler.Jacques00@gmail.com>
2026-03-01 15:56:55 -0500

    one shooter good

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -8,8 +8,8 @@ import edu.wpi.first.math.util.Units;
 
 public class ShooterConstants {
     public enum ShooterSpeeds {
-        kHubSpeed(4000.0),
-        kFeedSpeed(6000.0);
+        kHubSpeed(3000.0),
+        kFeedSpeed(3000.0);
 
         private double speedMPS;
         private double speedRPM;
@@ -39,18 +39,18 @@ public class ShooterConstants {
     public static final boolean kLeftShooterMotorInverted = true;
     public static final boolean kRightShooterMotorInverted = false;
 
-    public static final double kLeftP = 0;//0.1;
+    public static final double kLeftP = 0.0;//0.001;
     public static final double kLeftI = 0;
     public static final double kLeftD = 0;
     public static final double kLeftS = 0;
-    public static final double kLeftV = 0.0;
+    public static final double kLeftV = 0.00121;
     public static final double kLeftA = 0;
 
-    public static final double kRightP = 0;//0.1;
+    public static final double kRightP = 0.001;//0.1;
     public static final double kRightI = 0;
-    public static final double kRightD = 0;
+    public static final double kRightD = 0.000;
     public static final double kRightS = 0;
-    public static final double kRightV = 0.1;
+    public static final double kRightV = 0.00121;
     public static final double kRightA = 0;
 
     public static final double kMaxManualSpeedMultiplier = 1;
@@ -58,7 +58,7 @@ public class ShooterConstants {
     public static final double kShooterHeightMeters = 0;
 
     // TODO Is this value sane?
-    public static final int kCurrentLimit = 50;
+    public static final int kCurrentLimit = 60;
 
     public static final IdleMode kShooterIdleMode = IdleMode.kCoast;
 
